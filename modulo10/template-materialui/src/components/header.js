4ff044b9a4a175f2ddc0003a46ada997fb8d8624
Fábio Aranha <sabioaranha@gmail.com>
2024-11-11 13:45:00 -0300

    feat: alteracao nome do projeto

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -24,7 +24,7 @@ function Header() {
               */}
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
+              Atividade Prática - Gerenciador de Tarefas 
             </Typography>
           </Toolbar>
         </AppBar>
